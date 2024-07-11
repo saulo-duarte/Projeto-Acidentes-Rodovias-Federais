4281f407f9fb61cc0a8bba9b50cc05cc895804b8
saulo-duarte <saulolduarte@outlook.com>
2024-07-11 15:29:19 -0300

    Adiciona os gráficos de mapa de calor

--- pages/3📈_Detalhamento.py
+++ pages/3📈_Detalhamento.py
@@ -4,7 +4,7 @@ import streamlit as st
 import geopandas as gpd
 import pandas as pd
 import plotly.express as px
-import pymannkendall as mk
+from sqlalchemy import create_engine
 
 st.logo("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRo-jThbvam4VNl--sYlWeTE89wKpfWQE2NRQ&s")
 
@@ -24,52 +24,24 @@ st.markdown(
 )
 
 @st.cache_data
-def carregar_dados():
-    df1 = pd.read_csv("Dados_prf_simplificado_parte1.txt", low_memory=False)
-    df2 = pd.read_csv("Dados_prf_simplificado_parte2.txt", low_memory=False)
-    df = pd.concat([df1, df2], ignore_index=True)
-    return df
+def load_data():
+    DB_USER = st.secrets.db_credentials.POSTGRES_USER
+    DB_PASSWORD = st.secrets.db_credentials.POSTGRES_PASSWORD
+    DB_HOST = st.secrets.db_credentials.POSTGRES_HOST
+    DB_PORT = st.secrets.db_credentials.POSTGRES_PORT
+    DB_NAME = st.secrets.db_credentials.POSTGRES_DB 
 
+    connection_str = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+    engine = create_engine(connection_str)
 
-df = carregar_dados()
+    query_acidentes = """
+    SELECT * FROM relational.acidentes
+    INNER JOIN relational.envolvidos
+    ON acidentes.id = envolvidos.id_acidente;
+    """
 
-df = df[['id', 'data_inversa']]
-df['data_inversa'] = pd.to_datetime(df['data_inversa'])
+    df_acidentes = pd.read_sql(query_acidentes, con=engine)
+    return df_acidentes
 
-# criando a coluna de ano
+df = load_data()
 
-df['ano'] = df['data_inversa'].dt.year
-
-# Fazendo a contagem de acidentes por dia
-
-df = df.groupby('ano').size().reset_index(name='acidentes')
-
-# Fazendo teste de hipótese para verificar se a o aumento de acidentes é significativo
-
-# Criando uma tabela de contingência
-
-contingency_table = pd.crosstab(df['ano'], df['acidentes'])
-
-# Teste de hipótese
-
-chi2, p, dof, expected = chi2_contingency(contingency_table)
-
-# Verificando se a hipótese nula é rejeitada
-
-if p < 0.05:
-    st.write(f"A hipótese nula é rejeitada. O aumento de acidentes é significativo| {p}")
-else:
-    st.write(f"A hipótese nula não é rejeitada. O aumento de acidentes não é significativo|{p}")
-
-# Teste de tendência de Mann-Kendall
-
-result = mk.original_test(df['acidentes'])
-
-if result.slope > 0:
-    st.write("Há uma tendência de aumento de acidentes")
-else:
-    st.write("Não há uma tendência de aumento de acidentes")
-
-nomes = ['Tendência','Presença de tendência','p-valor','Estatística do teste','Tau Kendall','Pontuação Kendall','Variância S','Declive','Intercepto']
-for i,j in zip(nomes,result): 
-    st.write(i,":",j)
--- pages/4🌡️_Mapas_De_Calor.py
+++ pages/4🌡️_Mapas_De_Calor.py
@@ -7,35 +7,32 @@ import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
 import streamlit as st
-from matplotlib.colors import LinearSegmentedColormap
-from mpl_toolkits.basemap import Basemap
-from sklearn.neighbors import KDTree
+from sqlalchemy import create_engine
 
 st.set_page_config(layout="wide")
 
 @st.cache_data
-def carregar_dados():
-    df1 = pd.read_csv("Dados_prf_simplificado_parte1.txt", low_memory=False)
-    df2 = pd.read_csv("Dados_prf_simplificado_parte2.txt", low_memory=False)
-    df = pd.concat([df1, df2], ignore_index=True)
-    return df
+def load_data():
+    DB_USER = st.secrets.db_credentials.POSTGRES_USER
+    DB_PASSWORD = st.secrets.db_credentials.POSTGRES_PASSWORD
+    DB_HOST = st.secrets.db_credentials.POSTGRES_HOST
+    DB_PORT = st.secrets.db_credentials.POSTGRES_PORT
+    DB_NAME = st.secrets.db_credentials.POSTGRES_DB 
 
-@st.cache_data
-def carregar_mapa():
-    os.environ['SHAPE_RESTORE_SHX'] = 'YES'
-    file_path = "rodo_snv.shp"
-    rodovias = gpd.read_file(file_path)
-    return rodovias
+    connection_str = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+    engine = create_engine(connection_str)
 
+    query_acidentes = """
+    SELECT * 
+    FROM relational.acidentes
+    INNER JOIN relational.envolvidos
+    ON acidentes.id = envolvidos.id_acidente
+    """
 
-st.markdown("""
-    <style>
-        body {
-            zoom: 0.8;
-        }
-    </style>
-""", unsafe_allow_html=True)
+    df_acidentes = pd.read_sql(query_acidentes, con=engine)
+    return df_acidentes
 
+df = load_data()
 
 st.markdown("""
     <style>
@@ -53,8 +50,7 @@ st.markdown("""
 st.markdown("----")
 
 
-df = carregar_dados()
-rodovias = carregar_mapa()
+df = load_data()
 
 df_filtrado = df.copy()
 conn = duckdb.connect()
@@ -140,178 +136,23 @@ st.markdown(f'<div style="{square_style}"> \
 st.markdown("----")
 # ----- Gráfico 1 com rodovias
 
-raio_pesquisa_km1 = 5
 
-grid_size1 = 0.02
-df['lat_grid1'] = np.floor(df['latitude'] / grid_size1) * grid_size1
-df['lon_grid1'] = np.floor(df['longitude'] / grid_size1) * grid_size1
-
-unique_cells1 = df[['lat_grid1', 'lon_grid1']].drop_duplicates().values
-
-densidade_acidentes1 = np.zeros(len(unique_cells1))
-tree1 = KDTree(unique_cells1, leaf_size=10)
-
-for i, cell in enumerate(unique_cells1):
-    vizinhos = tree1.query_radius([cell], r=raio_pesquisa_km1)
-    densidade_acidentes1[i] = len(vizinhos[0])
-
-densidades_dict1 = {tuple(cell): densidade for cell, densidade in zip(unique_cells1, densidade_acidentes1)}
-df['densidade_acidentes1'] = df[['lat_grid1', 'lon_grid1']].apply(lambda row: densidades_dict1[tuple(row)], axis=1)
-
-plot_colors1 = [(0, "white"), 
-               (0.2, "#c4bb00"),  
-               (0.4, "#ff8000"),   
-               (0.6, "#ff5100"),  
-               (0.8, "#ff2a00"),
-               (1, "#4a0000")  
-              ]
-cmap1 = LinearSegmentedColormap.from_list("custom", plot_colors1)
-
-grafico1 = plt.figure(figsize=(10, 8))
-map = Basemap(llcrnrlon=-75, llcrnrlat=-35, urcrnrlon=-25, urcrnrlat=5, resolution='i', projection='merc')
-map.drawcoastlines()
-map.drawcountries()
-
-for geometry in rodovias['geometry']:
-    lon, lat = zip(*geometry.coords)
-    x, y = map(lon, lat)
-    map.plot(x, y, marker=None, color='navy', linewidth=1)
-
-x, y = map(df['longitude'].values, df['latitude'].values)
-sc = map.scatter(x, y, s=30, c=df['densidade_acidentes1'], cmap=cmap1, alpha=0.7, edgecolors='none',
-                 vmin=0, vmax=np.percentile(df['densidade_acidentes1'], 98))
-
-cbar = plt.colorbar(sc, orientation='vertical')
-plt.title('Acidentes em Rodovias Brasileiras')
-
-# ---------------- Gráfico 2
-
-raio_pesquisa_km2 = 0.03
-
-tree2 = KDTree(df[['latitude', 'longitude']].values)
-
-vizinhos_proximos2 = tree2.query_radius(df[['latitude', 'longitude']].values, r=raio_pesquisa_km2)
-
-densidade_acidentes2 = [len(x) for x in vizinhos_proximos2]
-
-df['densidade_acidentes_2'] = densidade_acidentes2
-grafico2 = go.Figure(go.Densitymapbox(
-    lat=df['latitude'], 
-    lon=df['longitude'], 
-    z=df['densidade_acidentes_2'],  
-    radius=10,
-    colorscale="YlOrRd",
-    zmin=0,
-    zmax=np.percentile(df['densidade_acidentes1'], 98)
-))
-grafico2.update_layout(
-    mapbox_style="carto-positron",
-    mapbox_center={"lat": -15, "lon": -55},
-    mapbox_zoom=3,
-    title={
-        'text': "Utilize zoom para mais precisão",
-        'font': {'size': 20}
-    },
-    margin={"r":50,"t":50,"l":50,"b":50},
-    width=700,  
-    height=650,
-    title_x=0.4,  
-)
-# ----------------- Estrutura Parte 1
-col1 , col2 = st.columns(2)
+col1, col2 = st.columns(2)
 with col1:
-     st.pyplot(grafico1, use_container_width=True)
+    st.image("pic/densidade_acidentes.png", use_column_width=True)
 with col2:
-     st.plotly_chart(grafico2, use_container_width=True)
+    with open('pic/grafico_densidade_acidentes.html', 'r') as file:
+        html_content = file.read()
+    st.components.v1.html(html_content, height=800)
 
 st.markdown("## Acidentes Com Mortos")
 st.markdown("------")
 
-# ------------------- Gráfico 3
-
-raio_pesquisa_km3 = 5
-
-grid_size3 = 0.02 
-df['lat_grid3'] = np.floor(df['latitude'] / grid_size3) * grid_size3
-df['lon_grid3'] = np.floor(df['longitude'] / grid_size3) * grid_size3
-
-df_filtered3 = df[df['mortos'] >= 1]
-
-unique_cells3 = df_filtered3[['lat_grid3', 'lon_grid3']].drop_duplicates().values
-
-densidade_acidentes3 = np.zeros(len(unique_cells3))
-tree3 = KDTree(unique_cells3, leaf_size=10)
-
-for i, cell in enumerate(unique_cells3):
-    vizinhos = tree3.query_radius([cell], r=raio_pesquisa_km3)
-    densidade_acidentes3[i] = len(vizinhos[0])
-
-densidades_dict3 = {tuple(cell): densidade for cell, densidade in zip(unique_cells3, densidade_acidentes3)}
-df_filtered3['densidade_acidentes'] = df_filtered3[['lat_grid3', 'lon_grid3']].apply(lambda row: densidades_dict3[tuple(row)], axis=1)
-
-plot_colors = [(0, "white"), 
-               (0.2, "#c4bb00"),  
-               (0.4, "#ff8000"),   
-               (0.6, "#ff5100"),  
-               (0.8, "#ff2a00"),
-               (1, "#4a0000")  
-              ]
-cmap = LinearSegmentedColormap.from_list("custom", plot_colors)
-
-grafico3 = plt.figure(figsize=(10, 8))
-map = Basemap(llcrnrlon=-75, llcrnrlat=-35, urcrnrlon=-25, urcrnrlat=5, resolution='i', projection='merc')
-map.drawcoastlines()
-map.drawcountries()
-
-for geometry in rodovias['geometry']:
-    lon, lat = zip(*geometry.coords)
-    x, y = map(lon, lat)
-    map.plot(x, y, marker=None, color='navy', linewidth=1)
-
-x, y = map(df_filtered3['longitude'].values, df_filtered3['latitude'].values)
-sc = map.scatter(x, y, s=30, c=df_filtered3['densidade_acidentes'], cmap=cmap, alpha=0.7, edgecolors='none',
-                 vmin=0, vmax=np.percentile(df_filtered3['densidade_acidentes'], 98))
-
-cbar = plt.colorbar(sc, orientation='vertical')
-plt.title('Acidentes Com Mortes em Rodovias Brasileiras')
-
-# --------------- Gráfico 4
-raio_pesquisa_km4 = 0.8
-
-df_filtered4 = df[df['mortos'] >= 1]
-
-tree4 = KDTree(df_filtered4[['latitude', 'longitude']].values)
-
-vizinhos_proximos4 = tree4.query_radius(df_filtered4[['latitude', 'longitude']].values, r=raio_pesquisa_km4)  # Corrigido para usar raio_pesquisa_km4
-
-densidade_acidentes4 = [len(x) for x in vizinhos_proximos4]
-
-df_filtered4['densidade_acidentes_4'] = densidade_acidentes4
-
-grafico4 = go.Figure(go.Densitymapbox(
-    lat=df_filtered4['latitude'], 
-    lon=df_filtered4['longitude'], 
-    z=df_filtered4['densidade_acidentes_4'], 
-    radius=10,
-    colorscale="YlOrRd",
-    zmin=0,
-    zmax=np.percentile(df_filtered3['densidade_acidentes'], 98) 
-    ))
-
-grafico4.update_layout(
-    mapbox_style="carto-positron",
-    mapbox_center={"lat": -15, "lon": -55},
-    mapbox_zoom=3,
-    title="Utilize zoom para mais precisão",
-    margin={"r":50,"t":50,"l":50,"b":50},
-    width=700,  
-    height=650,
-    title_x=0.4,  
-)
-
 col3, col4 = st.columns(2)
-
 with col3:
-    st.pyplot(grafico3, use_container_width=True)
+    st.image("pic/densidade_acidentes_com_mortos.png", use_column_width=True)
 with col4:
-     st.plotly_chart(grafico4, use_container_width=True)
+    with open('pic/grafico_densidade_acidentes_com_mortos.html', 'r') as file:
+        html_content = file.read()
+    st.components.v1.html(html_content, height=800)
+
